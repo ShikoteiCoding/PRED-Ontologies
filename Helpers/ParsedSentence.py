34b83b1bd7297a335925d9bc21851667a2b87560
yahuanchen <yahuan.chen@etu.univ-nantes.fr>
2021-01-24 20:20:58 +0100

    solved couple extraction error; added typing

--- Helpers/ParsedSentence.py
+++ Helpers/ParsedSentence.py
@@ -1,29 +1,32 @@
+from typing import List, Set, Optional
+
+
 class parsed_sentence:
     def __init__(self):
         self.id = -1
         self.words = []
         self.NPs = []
 
-    def get_root_indexes(self):
+    def get_root_indexes(self) -> List[int]:
         indexes = []
         for np in self.NPs:
             indexes.append(np.root_index)
         return indexes
 
-    def get_hypo_or_hyper_indexes(self, hypo_or_hyper):
+    def get_hypo_or_hyper_indexes(self, hypo_or_hyper: str) -> Set[int]:
         indexes = set()
         for NP in self.NPs:
             if NP.text == hypo_or_hyper:
                 indexes.add(NP.root_index)
         return indexes
 
-    def get_lemma_by_index(self, ind):
+    def get_lemma_by_index(self, ind: int) -> Optional[str]:
         for word in self.words:
             if word.index == ind:
                 return word.lemma
         return None
 
-    def get_sequence_representation(self, hypo_indexes = [], hyper_indexes = []):
+    def get_sequence_representation(self, hypo_indexes=[], hyper_indexes=[]) -> str:
         if len(self.NPs) == 0:
             return "[]"
         s = "["
@@ -56,10 +59,11 @@ class parsed_sentence:
                 if word.index == np.root_index:
                     if word_h == "hypo, " and parent == "hypo":
                         continue
-                    if word_h=="hypo, " or word_h == "hyper, ":
+                    if word_h == "hypo, " or word_h == "hyper, ":
                         s += "(NP, " + word_h + word.lemma + "_lemma, " + word.pos + ", " + word.dep_rel + ", " + parent + "_dep), "
                     else:
-                        s += "(" + np.text.replace(" ", "_") + "_label, NP, " + word_h + word.lemma + "_lemma, " + word.pos + ", " + word.dep_rel + ", " + parent + "_dep), "
+                        s += "(" + np.text.replace(" ",
+                                                   "_") + "_label, NP, " + word_h + word.lemma + "_lemma, " + word.pos + ", " + word.dep_rel + ", " + parent + "_dep), "
             else:
                 if word_h == "hypo, " and parent == "hypo":
                     continue
@@ -67,7 +71,7 @@ class parsed_sentence:
         s = s[:-2] + "]"
         return s
 
-    def get_sequence_representation_for_SHyP(self):
+    def get_sequence_representation_for_SHyP(self) -> str:
         if len(self.NPs) == 0:
             return ""
         s = "["
@@ -93,7 +97,8 @@ class parsed_sentence:
                     li_ind_dif.append(dif)
             if min(li_ind_dif) > 5:
                 return ""
-            included_indexes = list(range(min(min(hypo_indexes), min(hyper_indexes))-5, max(max(hypo_indexes), max(hyper_indexes))+5))
+            included_indexes = list(
+                range(min(min(hypo_indexes), min(hyper_indexes)) - 5, max(max(hypo_indexes), max(hyper_indexes)) + 5))
         else:
             return ""
             # included_indexes = list(range(0, 50))
@@ -116,14 +121,14 @@ class parsed_sentence:
             else:
                 parent_index = word.parent_index
                 parent = self.get_lemma_by_index(parent_index)
-                if parent == "" or parent == None:
+                if parent == "" or parent is None:
                     parent = word.parent
                 # parent = word.parent
 
             if np_i > len(self.NPs) - 1:
                 np_i = len(self.NPs) - 1
             np = self.NPs[np_i]
-            if word.index >= np.start and word.index <= np.end:
+            if np.start <= word.index <= np.end:
                 if word.index == np.end:
                     np_i += 1
                 if word.index == np.root_index:
@@ -141,11 +146,11 @@ class parsed_sentence:
         s = s[:-2] + "]"
         return s
 
-    def add_word(self, w, l, pos, i, par, parent_index, rel , ty):
+    def add_word(self, w: str, l: str, pos, i, par, parent_index, rel, ty) -> None:
         word = parsed_word(w, l, pos, i, par, parent_index, rel, ty)
         self.words.append(word)
 
-    def add_NP(self, np, root, ri, start, end):
+    def add_NP(self, np, root, ri, start, end) -> None:
         np2 = noun_phrase(np, root, ri, start, end)
         self.NPs.append(np2)
 
@@ -154,6 +159,7 @@ class parsed_sentence:
 
     def __str__(self):
         return " ".join([word.word for word in self.words])
+
 
 class parsed_word:
     def __init__(self, w, l, pos, i, par, parent_index, rel, ty):
@@ -170,7 +176,10 @@ class parsed_word:
         return str(self)
 
     def __str__(self):
-        return "(" + self.word + ", " + self.lemma + ", " + self.pos + ", " + str(self.index) + ", " + self.parent + ", " + str(self.parent_index) + ", " + self.dep_rel+ ", " + self.type + ")"
+        return "(" + self.word + ", " + self.lemma + ", " + self.pos + ", " + str(
+            self.index) + ", " + self.parent + ", " + str(
+            self.parent_index) + ", " + self.dep_rel + ", " + self.type + ")"
+
 
 class noun_phrase:
     def __init__(self, np, root, ri, start, end):
@@ -184,4 +193,5 @@ class noun_phrase:
         return str(self)
 
     def __str__(self):
-        return "(" + self.text + ", " + self.root + ", " + str(self.root_index) + ", " + str(self.start) + ", " + str(self.end) + ")"
+        return "(" + self.text + ", " + self.root + ", " + str(self.root_index) + ", " + str(self.start) + ", " + str(
+            self.end) + ")"
