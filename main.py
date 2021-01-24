e2242864ed725b9196fa7c447e062b6619caad2b
ShikoteiCoding <business.vitis@gmail.com>
2021-01-24 14:11:22 +0100

    Attempt to extract : error un spm_matching function (Helpers)

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from typing import List
 from Helpers import core_functions as cf
 from Helpers import SP_matching as spm
+from Helpers.HyperHypoCouple import HHCouple
+from Helpers import parsed_sentence as ps
 
 # Folders
 dataset_path = "Dataset/"
@@ -27,13 +29,37 @@ def parse_pattern_file(path_to_corpus: str) -> List[str]:
         list_of_patterns.append(line.replace('\n', ''))
     return list_of_patterns
 
+def extract_patterns(path_to_corpus: str, list_of_patterns: List[str], core_concepts: List[str], limit: int) -> List[HHCouple]:
+
+    # Count
+    count = 0
+
+    # Store the couples to return in a list
+    extracted_couples = []
+
+    # Loop over the sentences
+    for sentence in cf.get_sentences(path_to_corpus):
+
+        # Limit to run test
+        if count > limit:
+            return extracted_couples
+
+        # Loop over the patterns
+        for pattern in list_of_patterns:
+            (done, hh_couples, pattern) = spm.spm_matching(pattern, sentence) #  :return bool, List[HHCouple], pattern:
+
+            if done:
+                for hh_couple in hh_couples:
+                    #if hh_couple.hypernym in list_of_patterns:
+                    extracted_couples.append(hh_couple)
+
+        count += 1
+    return extracted_couples
+
 if __name__ == "__main__":
     list_of_patterns = parse_pattern_file(path_to_spm)
-
-    for sentence in cf.get_sentences(path_to_corpus):
-        print(sentence)
-
-    #spm_matching(pattern, parsed_sentence, mingap = 0, maxgap = 3)
-
+    core_concepts = ["music"]
+    iter1_couples = extract_patterns(path_to_corpus, list_of_patterns, core_concepts, 10)
+    print(iter1_couples)
 
 
